b64128e28f2af02b404d9cccea55de7fcd46486b
Ariane de Almeida Franchetto <nanifranchetto@gmail.com>
2024-10-14 15:34:23 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Calculator from './components/Calculator.vue';
+
+
+</script>
+
+<template>
+  <div class="main">
+    <Calculator />
+
+  </div>
+</template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  background-image: linear-gradient(135deg, #d67eaa 0, #cc75ac 16.67%, #bf6cae 33.33%, #b063b0 50%, #9e5cb3 66.67%, #8956b6 83.33%, #7153b9 100%);
+  height: 100vh;
+
+}
+</style>
